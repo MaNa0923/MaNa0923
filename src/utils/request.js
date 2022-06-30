2a7ccefd448c3d727a3b0dcf37d08f6fd481d512
MaNa0923 <1491343463@qq.com>
2022-06-30 18:44:31 +0800

    feat(navber): 头部头像点击下拉

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,32 +1,10 @@
-/**
- *
- * // v1 https://www.baidu.com
- *
- * // v2 https://www.jd.com
- *
- * // v3 https://www.taobao.com
- *
- *
- * 引入axios
- *
- * 创建axios实例对象
- *
- * 创建请求拦截器
- *
- * 创建响应拦截器
- *
- * 统一传参方式   处理 get请求方式依旧可以使用data方式传参
- *
- * 全局loading加载
- *
- * 处理路由切换接口重复请求
- *
- * 导出axios实例对象
- *
- */
-
-// 导入axios
 import axios from 'axios'
+
+import store from '../store'
+
+import router from '../router'
+
+import { isCheckTimeout } from './auth'
 
 import md5 from 'md5'
 
@@ -52,6 +30,15 @@ service.interceptors.request.use(
     config.headers.codeType = time
 
     // TODO 将token 通过请求头发送给后台
+    const token = store.getters.token
+    if (token) config.headers.Authorization = 'Bearer ' + token
+
+    if (token) {
+      if (isCheckTimeout()) {
+        store.dispatch('user/logout')
+        router.push('/login')
+      }
+    }
 
     return config
   },
@@ -77,12 +64,21 @@ service.interceptors.response.use(
       _showError(message)
       return Promise.reject(new Error(message))
     }
-
-    // TODO token过期状态  401 描述信息  无感知登录 无感知刷新
   },
   (error) => {
     // 关闭loading加载
     loading.close()
+
+    // TODO token过期状态  401 描述信息  无感知登录 无感知刷新
+    if (
+      error.response &&
+      error.response.data &&
+      error.response.data.code === 401
+    ) {
+      store.dispatch('user/lgout')
+      router.push('/login')
+    }
+
     // 响应失败进行信息提示
     _showError(error.message)
     return Promise.reject(error)
